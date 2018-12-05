41597d9f1756b49eeaf62f0c3341c6700baf868e
Diogo Pimenta <a75369@alunos.uminho.pt>
2018-12-05 14:24:14 +0000

    Satisfazer o decreto absolutista da Salete.
    Passar projecto para versão 8, fim do 'var'.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -5,18 +5,18 @@ import java.util.concurrent.ExecutionException;
 
 public class Client {
     public static void main(final String[] args) {
-        final var numOps = 100; // Total number of gets + puts
-        final var maxNumKeys = 20; // Maximum number of keys in a single request
+        final int numOps = 100; // Total number of gets + puts
+        final int maxNumKeys = 20; // Maximum number of keys in a single request
         //final var keyUpperBound = Integer.MAX_VALUE; // Upper bound of a given key
-        final var keyUpperBound = 10; // Upper bound of a given key
-        final var valueLength = 20; // Length of each byte[] in put request
+        final int keyUpperBound = 10; // Upper bound of a given key
+        final int valueLength = 20; // Length of each byte[] in put request
 
-        final var store = new Store(Integer.parseInt(args[0]));
-        final var random = new Random();
-        for (var i = 0; i < numOps; i++) {
-            final var op = random.nextInt(2);
-            final var numKeys = random.nextInt(maxNumKeys + 1);
-            final var keys = new ArrayList<Long>();
+        final Store store = new Store(Integer.parseInt(args[0]));
+        final Random random = new Random();
+        for (int i = 0; i < numOps; i++) {
+            final int op = random.nextInt(2);
+            final int numKeys = random.nextInt(maxNumKeys + 1);
+            final Collection<Long> keys = new ArrayList<Long>();
             for (int j = 0; j < numKeys; j++) {
                 keys.add((long) random.nextInt(keyUpperBound));
             }
@@ -29,9 +29,9 @@ public class Client {
                     }
                     break;
                 case 1: // Put
-                    final var values = new HashMap<Long, byte[]>();
+                    final Map<Long, byte[]> values = new HashMap<Long, byte[]>();
                     keys.forEach(k -> {
-                        final var temp = new byte[valueLength];
+                        final byte[] temp = new byte[valueLength];
                         for (int j = 0; j < valueLength; j++) {
                             temp[j] = (byte) random.nextInt(Byte.MAX_VALUE + 1);
                         }
